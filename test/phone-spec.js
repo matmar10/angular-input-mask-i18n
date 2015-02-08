7cd6a09088ad1936479baaa8e690051111d6b899
Matthew J. Martin <matthew.mar10@gmail.com>
2015-02-08 00:01:26 -0800

    add test case for US phone

--- test/phone-spec.js
+++ test/phone-spec.js
@@ -10,7 +10,7 @@ describe('ui.utils.masks.number', function() {
 	});
 
 	describe('ui-phone-number:', function() {
-		it('should apply a phone number mask while the user is typing:', function() {
+		it('should apply a phone number mask while the user is typing for country br', function() {
 			var BS = protractor.Key.BACK_SPACE;
 
 			var tests = [
@@ -48,6 +48,44 @@ describe('ui.utils.masks.number', function() {
 				expect(value.getText()).toEqual(tests[i].modelValue);
 			}
 		});
+
+        it('should apply a phone number mask while the user is typing for country us', function () {
+            var BS = protractor.Key.BACK_SPACE;
+
+            var tests = [
+                {key:'1', viewValue:'(1', modelValue:'1'},
+                {key:'2', viewValue:'(12', modelValue:'12'},
+                {key:'3', viewValue:'(123', modelValue:'123'},
+                {key:'4', viewValue:'(123) 4', modelValue:'1234'},
+                {key:'5', viewValue:'(123) 45', modelValue:'12345'},
+                {key:'6', viewValue:'(123) 456', modelValue:'123456'},
+                {key:'7', viewValue:'(123) 456-7', modelValue:'1234567'},
+                {key:'8', viewValue:'(123) 456-78', modelValue:'12345678'},
+                {key:'9', viewValue:'(123) 456-789', modelValue:'123456789'},
+                {key:'0', viewValue:'(123) 456-7890', modelValue:'1234567890'},
+                {key:'1', viewValue:'(123) 456-7890', modelValue:'1234567890'},
+                {key:'2', viewValue:'(123) 456-7890', modelValue:'1234567890'},
+                {key:BS, viewValue:'(123) 456-789', modelValue:'123456789'},
+                {key:BS, viewValue:'(123) 456-78', modelValue:'12345678'},
+                {key:BS, viewValue:'(123) 456-7', modelValue:'1234567'},
+                {key:BS, viewValue:'(123) 456', modelValue:'123456'},
+                {key:BS, viewValue:'(123) 45', modelValue:'12345'},
+                {key:BS, viewValue:'(123) 4', modelValue:'1234'},
+                {key:BS, viewValue:'(123', modelValue:'123'},
+                {key:BS, viewValue:'(12', modelValue:'12'},
+                {key:BS, viewValue:'(1', modelValue:'1'},
+                {key:BS, viewValue:'', modelValue:''}
+            ];
+
+            var input = element(by.model('phoneNumber2')),
+                value = element(by.binding('phoneNumber2'));
+
+            for (var i = 0; i < tests.length; i++) {
+                input.sendKeys(tests[i].key);
+                expect(input.getAttribute('value')).toEqual(tests[i].viewValue);
+                expect(value.getText()).toEqual(tests[i].modelValue);
+            }
+        });
 
 		it('should apply a phone number mask in a model with default value:', function() {
 			var BS = protractor.Key.BACK_SPACE;
